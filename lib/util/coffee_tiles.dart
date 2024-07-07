9f1197d3e514a6a51e0dc9adbb4c2267415c8a59
Filburb <filbertwijaya2003@gmail.com>
2024-07-07 12:02:11 +0700

    tt

--- lib/util/coffee_tiles.dart
+++ lib/util/coffee_tiles.dart
@@ -1,16 +1,24 @@
-import 'package:Trailblazer_Flutter/pages/detail_item/detail_item.dart';
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:Trailblazer_Flutter/pages/detail_item/detail_item.dart';
 import 'package:Trailblazer_Flutter/util/provider.dart';
 import 'package:provider/provider.dart';
 
 // ignore: must_be_immutable
-class coffeeTile extends StatelessWidget {
+class coffeeTile extends StatefulWidget {
   final coffeeType coffee_param;
   late int indexCoffee;
 
   coffeeTile({required this.coffee_param, required this.indexCoffee});
+
+  @override
+  _coffeeTileState createState() => _coffeeTileState();
+}
+
+class _coffeeTileState extends State<coffeeTile> {
+  bool _isAdding = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,13 +33,15 @@ class coffeeTile extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // image
           GestureDetector(
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => DetailItem(
-                        coffee: coffee_param,
-                        index: indexCoffee,
-                      )));
+                builder: (context) => DetailItem(
+                  coffee: widget.coffee_param,
+                  index: widget.indexCoffee,
+                ),
+              ));
             },
             child: SizedBox(
               width: MediaQuery.of(context).size.width,
@@ -40,80 +50,100 @@ class coffeeTile extends StatelessWidget {
                 alignment: Alignment.topRight,
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image:
-                          NetworkImage(coffee_param.detail[indexCoffee].image),
-                    ),
-                    borderRadius: BorderRadius.circular(12)),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(widget.coffee_param.detail[widget.indexCoffee].image),
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
               ),
             ),
           ),
-          //coffee name
+          // coffee name
           Expanded(
-              child: Padding(
-            padding: EdgeInsets.only(left: 5, top: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  coffee_param.name,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  coffee_param.detail[indexCoffee].nameDetail,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Color.fromARGB(255, 174, 174, 174),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      coffee_param.detail[indexCoffee].price,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                        fontFamily: "Sora",
-                        fontWeight: FontWeight.bold,
-                      ),
+            child: Padding(
+              padding: EdgeInsets.only(left: 5, top: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    widget.coffee_param.name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        CoffeeNewProvider coffeeProvider =
-                            Provider.of<CoffeeNewProvider>(context,
-                                listen: false);
-                        coffeeProvider.addItemToSelected(
-                            coffee_param, indexCoffee);
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Item successfully added'),
-                            duration: Duration(seconds: 2)));
-                      },
-                      child: Container(
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 198, 124, 74),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: const Icon(
-                          Icons.add,
-                          color: Colors.white,
+                  ),
+                  Text(
+                    widget.coffee_param.detail[widget.indexCoffee].nameDetail,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromARGB(255, 174, 174, 174),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.coffee_param.detail[widget.indexCoffee].price,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontFamily: "Sora",
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                      GestureDetector(
+                        onTap: () {
+                          _addItemToCheckout(context);
+                        },
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: _isAdding ? Color.fromARGB(255, 131, 75, 38) : Color.fromARGB(255, 198, 124, 74),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          )),
+          ),
         ],
       ),
     );
+  }
+
+  void _addItemToCheckout(BuildContext context) {
+    if (!_isAdding) {
+      setState(() {
+        _isAdding = true;
+      });
+
+      CoffeeNewProvider coffeeProvider = Provider.of<CoffeeNewProvider>(context, listen: false);
+      coffeeProvider.addItemToSelected(widget.coffee_param, widget.indexCoffee);
+
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Berhasil menambahkan item ke checkout"),
+          duration: Duration(seconds: 2),
+        ),
+      );
+
+      Timer(Duration(seconds: 1), () {
+        setState(() {
+          _isAdding = false;
+        });
+      });
+    }
   }
 }
